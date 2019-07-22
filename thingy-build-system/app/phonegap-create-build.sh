795dc9074dce80c78ffbb2d14046f727fc6f5556
Lenard Frommelt <lenny09918050@googlemail.com>
2019-07-22 15:34:41 +0200

    fixed softlinks to include res and scripts folders

--- thingy-build-system/app/phonegap-create-build.sh
+++ thingy-build-system/app/phonegap-create-build.sh
@@ -2,5 +2,5 @@
 rm -r toolset/local-build
 #phonegap create local-build --id 'com.auroxtech.headbandapp' --name 'Aurox Headband App' --link-to aurox-app-phonegap-build/www
 phonegap create toolset/local-build --link-to toolset/cordova-source
-ln -s toolset/cordova-source/res toolset/local-build/res
-ln -s toolset/cordova-source/scripts toolset/local-build/scripts
+ln -s ../cordova-source/res toolset/local-build/res
+ln -s ../cordova-source/scripts toolset/local-build/scripts
