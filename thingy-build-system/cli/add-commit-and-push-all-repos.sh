a32df21aa2019ffed4e21c664180028f6134dc23
Lenard Frommelt <lenny09918050@googlemail.com>
2019-07-25 12:49:06 +0200

    updated push and pull scripts to be usable

--- thingy-build-system/cli/add-commit-and-push-all-repos.sh
+++ thingy-build-system/cli/add-commit-and-push-all-repos.sh
@@ -1,4 +1,25 @@
 #!/bin/bash
+cd output
+git add .
+git commit -am "$1"
+git push origin master
+
+cd ../toolset
+git add .
+git commit -am "$1"
+git push origin master
+
+cd ../sources
+git add .
+git commit -am "$1"
+git push origin master
+
+cd ../testing
+git add .
+git commit -am "$1"
+git push origin master
+
+cd ..
 git add .
 git commit -am "$1"
 git push origin master
--- thingy-build-system/cli/pull-all.sh
+++ thingy-build-system/cli/pull-all.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
-git pull
+git checkout master
+git pull origin master
+cd ../toolset
+git checkout master
+git pull origin master
+cd sources
+git checkout master
+git pull origin master
+cd ../testing
+git checkout master
+git pull origin master
+# cd ../output
+# git checkout master
+# git pull origin master
 
 echo 0
