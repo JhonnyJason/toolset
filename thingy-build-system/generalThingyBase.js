230a2146205bbe092fa72e4a9cc650cad4806166
Lenard Frommelt <lenny09918050@googlemail.com>
2020-03-22 10:20:44 +0100

    0

--- thingy-build-system/generalThingyBase.js
+++ thingy-build-system/generalThingyBase.js
@@ -103,9 +103,9 @@ const getBaseDependencies = ()  => {
         "coffeescript": "^2.5.1",
         "npm-check-updates": "^4.0.4",
         "npm-run-all": "^4.1.5",
-        "thingy-allmodules-sync": "^0.1.2",
-        "thingy-module-gen": "^0.1.3",
-        "thingymodulecreate": "^0.1.6",
+        "thingy-allmodules-sync": "^0.1.3",
+        "thingy-module-gen": "^0.1.4",
+        "thingymodulecreate": "^0.1.7",
         "thingysync": "^0.1.1"
     }
 }
