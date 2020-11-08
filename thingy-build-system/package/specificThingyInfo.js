f376fc3e1796345ab0a8868316f396a66d2bf952
Lenard Frommelt <lenny09918050@googlemail.com>
2020-11-08 13:43:46 +0100

    2

---
+++ thingy-build-system/package/specificThingyInfo.js
@@ -0,0 +1,76 @@
+const fs = require("fs")
+const pathModule = require("path")
+
+const jsDest = "output/"
+const coffeeSource = "sources/source/*/*.coffee"
+
+//#region shellscrip paths
+const copyScript = "sources/ressources/copyscript.sh"
+
+const base = "toolset/thingy-build-system/package/"
+const updatePackagesScript = base + "update-packages.sh"
+const publishScript = base + "publish-on-npm.sh"
+const cleanPackageScript = base + "clean-package.sh"
+const installNodeModulesScript = base + "install-node-modules.sh"
+const incVersionsScript = base + "inc-versions.js"
+const syncVersionsScript = base + "sync-versions.js"
+const updateToolsScript = base + "update-tools.sh"
+//#endregion
+
+var sourceInfo = null
+try {
+    sourceInfo = require("./sourceInfo")
+} catch(err) { 
+    console.log(err.message)
+}
+
+// console.log("sourceInfo is: " + sourceInfo)
+
+module.exports = {
+    type: "cli",
+    getScripts: () => {
+        return {
+            //general Base expects this script and calls it on postinstall
+            "initialize-thingy": "run-s -ns build",
+            
+            // overwrite the general base stuff
+            "build-coffee": "coffee -o " + jsDest + " -c " + coffeeSource,
+            "watch-coffee": "coffee -o " + jsDest + " -cw " + coffeeSource,
+            "sync-allmodules": "thingy-allmodules-sync --style require",
+
+            //For testing and building
+            // "test": "run-s -ns build watch",
+            "build": "run-s -ns clean-package build-coffee copyscript install-node-modules",
+            "watch": "run-p -nsr watch-coffee",
+            
+            //for release
+            "release": "run-s -ns build publish-script",
+
+            "package-update-release": "run-s -ns update-tools update-packages inc-versions update-cli-packages push release",
+
+            // shellscripts to be called
+            "inc-versions": incVersionsScript,
+            "sync-versions": syncVersionsScript,
+            "update-tools": updateToolsScript,
+            "update-cli-packages": updatePackagesScript,
+            "prepend-shebang": prependShebangScript,
+            "publish-script": publishScript,
+            "clean-package": cleanPackageScript,
+            "install-node-modules": installNodeModulesScript,
+            "copyscript": copyScript
+        }
+    },
+    getDependencies: () => {
+        
+        var thingyDeps = { }
+
+        if(sourceInfo) {
+            Object.assign(thingyDeps, sourceInfo.getDependencies())
+        }
+        return thingyDeps
+
+    },
+    produceConfigFiles: (projectRoot) => {
+        return
+    }
+}
