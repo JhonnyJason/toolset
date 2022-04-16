afce423899b52bdb164b89e30651c6287a8ef424
Lenard Frommelt <lenny09918050@gmail.com>
2022-04-16 12:45:32 +0200

    0

--- thingy-build-system/generalThingyBase.js
+++ thingy-build-system/generalThingyBase.js
@@ -101,7 +101,7 @@ const getHomepage = (remoteURL) => {
 const getBaseDependencies = ()  => {
     return {
         "coffeescript": "^2.6.1",
-        "npm-check-updates": "^12.5.8",
+        "npm-check-updates": "^12.5.9",
         "npm-run-all": "^4.1.5",
         "thingy-allmodules-sync": "^0.1.4",
         "thingy-module-gen": "^0.1.5",
