09743ff6c64339282b52bea9b5bd00b544627657
Lenard Frommelt <lenny09918050@gmail.com>
2023-07-09 12:16:54 +0300

    2

--- thingy-build-system/admin-pwa/specificThingyInfo.js
+++ thingy-build-system/admin-pwa/specificThingyInfo.js
@@ -180,7 +180,7 @@ module.exports = {
             "pug-cli": "^1.0.0-alpha6",
             "purgecss": "^4.1.3",
             "stylus": "^0.59.0",
-            "webpack": "^5.88.0",
+            "webpack": "^5.88.1",
             "webpack-cli": "^5.1.4"
         }
 
