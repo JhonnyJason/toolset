42acdc4c8f009b1582d5064f5d53069f397ec3e6
Lenard Frommelt <lenny09918050@gmail.com>
2023-07-19 15:19:48 +0300

    2

--- thingy-build-system/machine/specificThingyInfo.js
+++ thingy-build-system/machine/specificThingyInfo.js
@@ -88,7 +88,7 @@ module.exports = {
         
         var thingyDeps = {
             "mustache": "^4.2.0",
-            "webpack": "^5.88.1",
+            "webpack": "^5.88.2",
             "webpack-cli": "^5.1.4",
             "generate-nginx-config-for-thingies": "^0.1.9",
             "generate-service-files-for-thingies": "^0.1.4",
--- thingy-build-system/service/specificThingyInfo.js
+++ thingy-build-system/service/specificThingyInfo.js
@@ -69,7 +69,7 @@ module.exports = {
     getDependencies: () => {
         
         var thingyDeps = {
-            "webpack": "^5.88.1",
+            "webpack": "^5.88.2",
             "webpack-cli": "^5.1.4"
         }
 
