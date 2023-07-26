92cf963e3d05924116b6690d47c8bb72796b83b0
Lenard Frommelt <lenny09918050@gmail.com>
2023-07-26 08:58:18 +0300

    2

--- thingy-build-system/generalThingyBase.js
+++ thingy-build-system/generalThingyBase.js
@@ -112,7 +112,7 @@ const getBaseDependencies = ()  => {
     return {
         "coffeescript": "^2.7.0",
         "livescript": "^1.6.0",
-        "npm-check-updates": "^16.10.16",
+        "npm-check-updates": "^16.10.17",
         "npm-run-all": "^4.1.5",
         "thingy-allmodules-sync": "^0.2.0",
         "thingy-module-gen": "^0.1.6",
