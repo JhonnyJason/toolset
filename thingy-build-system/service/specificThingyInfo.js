2bd3b98da7e8538fc9c7b67f88a90f3ad52b5d6c
Lenard Frommelt <lenny09918050@gmail.com>
2024-09-26 15:20:19 +0300

    2

--- thingy-build-system/service/specificThingyInfo.js
+++ thingy-build-system/service/specificThingyInfo.js
@@ -69,7 +69,7 @@ module.exports = {
     getDependencies: () => {
         
         var thingyDeps = {
-            "webpack": "^5.93.0",
+            "webpack": "^5.95.0",
             "webpack-cli": "^5.1.4"
         }
 
