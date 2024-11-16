13a704e8cdea852961a22f38c4272b41f286188b
Lenard Frommelt <lenny09918050@gmail.com>
2024-11-16 17:09:20 +0300

    0

--- thingy-build-system/cli/update-packages.sh
+++ thingy-build-system/cli/update-packages.sh
@@ -3,10 +3,10 @@ rm -r output/node_modules
 rm output/package-lock.json
 
 ncu -u --cwd output
-# cd output
-# ../node_modules/npm-check-updates/build/src/bin/cli.js -u
-# pnpm install
-# cd ..
+
+cd output
+pnpm install
+cd ..
 
 cp output/package.json sources/ressources/package.json
 
