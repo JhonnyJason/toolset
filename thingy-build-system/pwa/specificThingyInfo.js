7834b472e02f70a2b8939266f25b29060977f3ae
Lenard Frommelt <lenny09918050@gmail.com>
2025-05-20 18:05:43 +0300

    2

--- thingy-build-system/pwa/specificThingyInfo.js
+++ thingy-build-system/pwa/specificThingyInfo.js
@@ -169,7 +169,7 @@ module.exports = {
             "pug-cli": "^1.0.0-alpha6",
             "purgecss": "^7.0.2",
             "stylus": "^0.64.0",
-            "webpack": "^5.99.8",
+            "webpack": "^5.99.9",
             "webpack-cli": "^6.0.1"
         }
 
--- thingy-build-system/service/specificThingyInfo.js
+++ thingy-build-system/service/specificThingyInfo.js
@@ -69,7 +69,7 @@ module.exports = {
     getDependencies: () => {
         
         var thingyDeps = {
-            "webpack": "^5.99.8",
+            "webpack": "^5.99.9",
             "webpack-cli": "^6.0.1"
         }
 
